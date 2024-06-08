dc2e4ce4f57c95a576380496feab012bb4a1f55a
JingLingee <167772028+JingLingee@users.noreply.github.com>
2024-06-08 11:09:43 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
 import discord
 from discord.ext import commands
 
-Token = open("token", "r").readline()
+Token = open("token.txt", "r").readline()
 
 intents = discord.Intents.default()
 intents.members = True
 intents.messages = True
 
-bot = commands.Bot(command_prefix='!!', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name}이(가) 성공적으로 로그인했습니다!')
 
 @bot.command()
-async def JingJing(ctx):
-    await ctx.send("나는 뀽뀽이다.")
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    
-    user_text = message.content
-    
-    await message.channel.send(user_text)
-
-    await bot.process_commands(message)
+async def c(ctx,*,text):
+    await ctx.send(text)
 
 bot.run(Token)
